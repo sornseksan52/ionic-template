4a1fb270b629bed647c8da0a4df1e77937509e7b
seksan mankhong <seksan_52@hotmail.com>
2018-07-01 16:53:02 +0700

    combind menu compleate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,32 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TabHomePage } from '../pages/tab-home/tab-home';
+import { TabCoursePage } from '../pages/tab-course/tab-course';
+import { TabContactPage } from '../pages/tab-contact/tab-contact';
+import { TabArticlePage } from '../pages/tab-article/tab-article';
+import { TabServicePage } from '../pages/tab-service/tab-service';
+import { SidePaymentPage } from '../pages/side-payment/side-payment';
+import { SidePortfolioPage } from '../pages/side-portfolio/side-portfolio';
+import { SideSchedulePage } from '../pages/side-schedule/side-schedule';
+import { SideSettingPage } from '../pages/side-setting/side-setting';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    TabHomePage,
+    TabCoursePage,
+    TabContactPage,
+    TabArticlePage,
+    TabServicePage,
+    SidePaymentPage,
+    SidePortfolioPage,
+    SideSchedulePage,
+    SideSettingPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -22,8 +37,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    TabHomePage,
+    TabCoursePage,
+    TabContactPage,
+    TabArticlePage,
+    TabServicePage,
+    SidePaymentPage,
+    SidePortfolioPage,
+    SideSchedulePage,
+    SideSettingPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
